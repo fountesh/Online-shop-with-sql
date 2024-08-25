51d1ba0c0edd15bfb74533e2f2f10ca3f56c4fc6
fountesh <128893715+fountesh@users.noreply.github.com>
2024-08-25 14:02:02 +0300

    version 1.0

--- main.py
+++ main.py
@@ -36,3 +36,37 @@ CREATE TABLE IF NOT EXISTS orders(
 
 conn.commit()
 
+cursor.executemany('''
+INSERT INTO products(product_id, name, category, price) 
+VALUES (?, ?, ?, ?)
+''', [
+    (101, "Lenovo", "Техніка", 24000),
+    (102, "Redmi", "Смартфони", 4000),
+    (103, "Samsung Galaxy s1", "Планшети", 14000),
+    (104, "JBL", "Навушники", 500),
+    (105, "Philips", "Холодильник", 24000)
+])
+
+cursor.executemany('''
+INSERT INTO customers(customer_id, first_name, last_name, email)
+VALUES (?, ?, ?, ?)
+''',[
+    (1, "Тарас", "Шевченко", "batkonashbandera@gmail.com"),
+    (2, "Іван", "Семиволосов", "batkonashbandera@gmail.com"),
+    (3, "Анна", "Ковалевська", "batkonashbandera@gmail.com"),
+    (4, "Олексій", "Трамп", "batkonashbandera@gmail.com"),
+    (5, "Павло", "Груша", "batkonashbandera@gmail.com"),
+])
+
+cursor.executemany('''
+INSERT INTO orders(order_id, customer_id, product_id, quantity, order_data)
+VALUES (?, ?, ?, ?, ?)
+''', [
+    (201, 2, 101, 1, "25.08.24"),
+    (202, 5, 104, 100, "26.08.24"),
+    (203, 3, 105, 3, "19.08.24"),
+    (204, 1, 102, 25, "25.07.24"),
+    (205, 4, 103, 6, "25.08.23")
+])
+
+conn.commit()
